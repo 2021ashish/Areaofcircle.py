effb3b81131c543aa78f1065d2762646cfe05cc1
Ashish kumar <87988665+2021ashish@users.noreply.github.com>
2021-07-27 01:50:03 +0530

    Create Areaofcircle.py

---
+++ Areaofcircle.py
@@ -0,0 +1,15 @@
+#area of circle
+
+a=float(input()) 
+
+pi=3.14
+
+area= pi*a*a 
+
+print(area)
+
+filename = input("Input the Filename: ")
+
+f_extns = filename.split(".")
+
+print ("The extension of the file is : " + repr(f_extns[-1]))
